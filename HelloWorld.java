8c8ec47fb2597a3db6b7fd549f65e9b8d0ed2e3b
aws.sandy.devops <aws.sandy.devops@gmail.com>
2023-02-25 12:42:21 +0000

    Removed some lines...

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,17 +10,6 @@ class HelloWorld
 		System.out.println("Hello, World");
 		System.out.println("Hello, World");
 		System.out.println("Hello, World");
-		System.out.println("Hello, World");
-		System.out.println("Hello, World");
-		System.out.println("Hello, World");
-		System.out.println("Hello, World");
-		System.out.println("Hello, World");
-		System.out.println("Hello, World");
-		System.out.println("Hello Guys, Welcome to DEVOPS");
-		System.out.println("Hello Guys, Welcome to DEVOPS");
-		System.out.println("Hello Guys, Welcome to DEVOPS");
-		System.out.println("Hello Guys, Welcome to DEVOPS");
-		System.out.println("Hello Guys, Welcome to DEVOPS");
 		System.out.println("Hello Guys, Welcome to DEVOPS");
 		System.out.println("Hello Guys, Welcome to DEVOPS");
 		System.out.println("Hello Guys, Welcome to DEVOPS");
